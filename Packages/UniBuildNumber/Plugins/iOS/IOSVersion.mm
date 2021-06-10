6c9656ff07d356ff7b151a9b64d7d21aa18cb0e7
Nakaji Kohki <boomerang.cube@gmail.com>
2021-06-10 23:21:21 +0900

    Add initial files

---
+++ Packages/UniBuildNumber/Plugins/iOS/IOSVersion.mm
@@ -0,0 +1,7 @@
+﻿extern "C" {
+    char *GetBundleVersion() {
+        NSString *bundleVersion = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
+        const char *s = [bundleVersion UTF8String];
+        return strcpy((char *)malloc(strlen(s) + 1), s);
+    }
+}
---
+++ Packages/UniBuildNumber/UniBuildNumber.cs
@@ -0,0 +1,59 @@
+﻿#if UNITY_IOS
+using System.Runtime.InteropServices;
+#endif
+#if UNITY_EDITOR
+using UnityEditor;
+
+#endif
+
+namespace nkjzm
+{
+    public static class UniBuildNumber
+    {
+        public static string GetBuildNumber()
+        {
+#if UNITY_EDITOR
+            return PlayerSettings.iOS.buildNumber;
+#elif UNITY_IOS
+            return GetBundleVersion();
+#else
+            return string.Empty;
+#endif
+        }
+
+        public static int GetVersionCode()
+        {
+#if UNITY_EDITOR
+            return PlayerSettings.Android.bundleVersionCode;
+#elif UNITY_ANDROID
+            using (var packageInfo = GetPackageInfo())
+            {
+                return packageInfo.Get<int>("versionCode");
+            }
+#else
+            return 0;
+#endif
+        }
+
+#if UNITY_IOS
+        [DllImport("__Internal")]
+        static extern string GetBundleVersion();
+#endif
+
+#if UNITY_ANDROID
+        static AndroidJavaObject GetPackageInfo()
+        {
+            using (var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+            using (var currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
+            using (var context = currentActivity.Call<AndroidJavaObject>("getApplicationContext"))
+            using (var packageManager = context.Call<AndroidJavaObject>("getPackageManager"))
+            using (var packageManagerClass = new AndroidJavaClass("android.content.pm.PackageManager"))
+            {
+                string packageName = context.Call<string>("getPackageName");
+                int activities = packageManagerClass.GetStatic<int>("GET_ACTIVITIES");
+                return packageManager.Call<AndroidJavaObject>("getPackageInfo", packageName, activities);
+            }
+        }
+#endif
+    }
+}
